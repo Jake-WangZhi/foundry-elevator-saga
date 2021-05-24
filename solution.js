45cdb97d3f65817931e484dc4fd287567162f4c2
Jake Wang <wang7367@umn.edu>
2021-05-24 00:08:02 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
 {       
-    //Challenge #7. Passing rate: 70%, failure cases mostly need to transport five more person to pass
+    //Challenge #7. Passing rate: 65%
 
     //same as the solution of challenge #4
     init: function(elevators, floors) {
